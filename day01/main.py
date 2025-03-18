b4a6a86bf67561b8101caa36771d3cbf099db175
Cleiton Oliveira <oliveira.cleiton@outlook.com.br>
2025-03-18 17:05:21 -0300

    added project name

--- day01/main.py
+++ day01/main.py
@@ -1,3 +1,5 @@
+#day 01, project Band Name Generetor.
+
 print("Welcome to the Band Name Generator.")
 city_name = input("What's the name of the city  you grew up in?\n")
 pet_name = input("What's your pet's name?\n")
